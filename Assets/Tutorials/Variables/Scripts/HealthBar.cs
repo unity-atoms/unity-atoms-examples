dad44850642c72cf84912a9dfcd03c26195df25f
Miika Lönnqvist <miika.lonnqvist@gmail.com>
2021-04-02 10:37:57 +0200

    Add contents for the Variables tutorial

---
+++ Assets/Tutorials/Variables/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityAtoms.BaseAtoms;
+
+namespace Tutorials.Variables
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField]
+        private IntVariable Health;
+        [SerializeField]
+        private IntConstant MaxHealth;
+
+        void Update()
+        {
+            GetComponent<Image>().fillAmount = 1.0f * Health.Value / MaxHealth.Value;
+        }
+    }
+}
