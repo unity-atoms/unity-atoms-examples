7c7779e35ea6a99374761d688399575478527615
Miika Lönnqvist <miika.lonnqvist@gmail.com>
2021-04-10 09:14:54 +0200

    Add contents for the Events tutorial

---
+++ Assets/Tutorials/Events/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityAtoms.BaseAtoms;
+
+namespace Tutorials.Events
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField]
+        private IntEvent HealthChangedEvent;
+        [SerializeField]
+        private IntConstant MaxHealth;
+
+        void Start()
+        {
+            HealthChangedEvent.Register(this.ChangeFillAmount);
+        }
+
+        void OnDestroy()
+        {
+            HealthChangedEvent.Unregister(this.ChangeFillAmount);
+        }
+
+        private void ChangeFillAmount(int health)
+        {
+            GetComponent<Image>().fillAmount = 1.0f * health / MaxHealth.Value;
+        }
+    }
+}
---
+++ Assets/Tutorials/Events/Scripts/PlayerHealth.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+namespace Tutorials.Events
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        public IntVariable Health;
+    }
+}
